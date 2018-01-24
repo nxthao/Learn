4c179d1fe08eb3ffcf854a27514fd6a4bfc08e91
nxthao <xthaoqt@gmail.com>
2018-01-24 16:28:53 +0700

    secondary commit

--- Learn_C/ex1.c
+++ Learn_C/ex1.c
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
     puts("Hello world.");
+    puts("Hello Thao Nguyen.");
     return 0;
 }
 
---
+++ Learn_C/ex3.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h> 
+
+int main()
+{
+    int age = 23;
+    int height = 165;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d cm tall.\n", height);
+
+    return 0;
+} 
