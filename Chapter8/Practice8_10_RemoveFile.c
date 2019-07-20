bd8b28ae544530e3bd8e8d5fa42e93c332826829
TN <nxthao0701@gmail.com>
2019-07-20 23:33:41 +0700

    Remove file

---
+++ Chapter8/Practice8_10_RemoveFile.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+/*
+Find the size of the file
+*/
+
+FILE *fp;
+errno_t err;
+int size;
+int res;
+char *data;
+char *data_file;
+char filename[] = "test.txt";
+
+char *InputData()
+{
+   static char data_tmp[100];
+   printf("Please input data\n");
+   fflush(stdin);
+   gets(data_tmp);
+   return data_tmp;
+}
+
+char WriteFile(char f_name[100], char data[100])
+{
+   err = fopen_s(&fp, f_name, "w");
+   if (err == 0)
+   {
+      if (strlen(data) > 0)
+      {
+         printf("The file is opened\n");
+         fputs(data, fp);
+         fputs("\n", fp);
+		 printf("data write %s \n", data);
+      }
+   }
+   else
+   {
+      printf("The file is unable to open\n");
+   }
+   fclose(fp);
+}
+
+char *ReadFile(char filename[10])
+{
+   static char data_read[100];
+   err = fopen_s(&fp, filename, "r");
+   if (err == 0)
+   {
+      printf("The file is opened\n");
+      fgets(data_read, 100, fp);
+      printf("data read: %s\n", data_read);
+   }
+   else
+   {
+      printf("The file is unable to open\n");
+   }
+   fclose(fp);
+   return data_read;
+}
+
+int findSize(char f_name[10])
+{
+   int res;  
+   err = fopen_s(&fp, f_name, "r");
+   if (err == 0)
+   {     
+      printf("The file is opened\n");
+      // Point to the end of the file
+      fseek(fp, 0, SEEK_END);
+      // Find size of file with ftell
+      res = ftell(fp);
+   }
+   else
+   {
+      printf("The file is unable to open\n");
+   }
+   return res;
+   fclose(fp);
+}
+
+int RemoveFile(char f_name[10])
+{
+   int res;
+   res = remove(f_name);
+   printf("res: %d\n", res);
+   if ( res == 0)
+   {
+      return 0;
+   }
+   else
+   {
+      return 1;
+   }
+}
+
+void main()
+{
+   data = InputData();
+
+   WriteFile(filename, data);
+
+   data_file = ReadFile(filename, data_file);
+   printf("data of file: %s\n", data_file);
+
+   size = findSize(filename);
+   printf("The size of the file: %d\n", (size-2));
+
+   res = RemoveFile(filename);
+
+   if (res == 0)
+   {
+      printf("Remove file successfully\n");
+   }
+   else
+   {
+      printf("The file is unable to remove\n");
+   }
+}
