608d85b9be4b301e6c5910ecfc803a9364433351
TN <nxthao0701@gmail.com>
2019-08-02 23:44:16 +0700

    Queue with Array

--- Chapter9/Practice9_4_QueueArray.c
+++ Chapter9/Practice9_4_QueueArray.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /*
 Queue with array: Enqueue, Dequeue, FrontQueue, RearQueue
-
 */
 
 const int MAX = 5;
 
 typedef struct
 {
-	int front;
-	int rear;
-	int size;
-	int data[];
+   int front;
+   int rear;
+   int size;
+   int data[];
 }Queue_t;
 
 Queue_t *CreateQueue()
@@ -43,20 +42,20 @@ void Enqueue(Queue_t *tmpQueue, int newData)
    }
    else
    {
-      if (tmpQueue->front = -1)
+      if (tmpQueue->front == -1)
       {
          tmpQueue->front = 0;
       }
       tmpQueue->rear++;
       tmpQueue->data[tmpQueue->rear] = newData;
       tmpQueue->size++;
-	  printf("%d is enqueue\n", tmpQueue->data[tmpQueue->front]);
+      printf("%d is enqueue\n", tmpQueue->data[tmpQueue->rear]);
    }
 }
 
 void Dequeue(Queue_t *tmpQueue)
 {
-   if (IsEmpty)
+   if (IsEmpty(tmpQueue))
    {
       printf("The queue is empty\n");
       //exit 0;
@@ -66,8 +65,41 @@ void Dequeue(Queue_t *tmpQueue)
       printf("%d is dequeue\n", tmpQueue->data[tmpQueue->front]);
       tmpQueue->front++;
       tmpQueue->size--;
+      if (tmpQueue->front > tmpQueue->rear)
+      {
+         tmpQueue->front = tmpQueue->rear = -1;
+      }
    }
+}
 
+void FrontQueue(Queue_t *tmpQueue)
+{
+
+   printf("The element of front: %d\n", tmpQueue->data[tmpQueue->front]);
+
+}
+
+void RearQueue(Queue_t *tmpQueue)
+{
+
+   printf("The element of rear: %d\n", tmpQueue->data[tmpQueue->rear]);
+
+}
+void Display(Queue_t *tmpQueue)
+{
+   if (IsEmpty(tmpQueue))
+   {
+      printf("The queue is empty\n");
+      //exit 0;
+   }
+   else
+   {
+      printf("The list queue is:\n");
+      for (int i = tmpQueue->front; i <= tmpQueue->rear; i++)
+      {
+         printf("%d \n", tmpQueue->data[i]);
+      }
+   }
 }
 
 void main()
@@ -75,6 +107,19 @@ void main()
    Queue_t *queue = CreateQueue();
    Enqueue(queue, 2);
    Enqueue(queue, 4);
+   Enqueue(queue, 6);
+   Enqueue(queue, 7);
+   FrontQueue(queue);
+   Enqueue(queue, 8);
+   Enqueue(queue, 9);
+   RearQueue(queue);
+   Enqueue(queue, 10);
+   Display(queue);
+
    Dequeue(queue);
+   Dequeue(queue);
+   FrontQueue(queue);
+   RearQueue(queue);
+   Display(queue);
 
 }
