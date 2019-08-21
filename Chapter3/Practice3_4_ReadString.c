cef8572c6c2ba3d0106c4454135739dfae06b8ea
tnqt <nxthao0701@gmail.com>
2019-08-21 22:50:00 +0700

    Read and display string with gets and puts

--- Chapter3/Practice3_4_ReadString.c
+++ Chapter3/Practice3_4_ReadString.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+/*
+Should use scanf_s to take number
+and take string by gets from stdin ( Can refer Practice6_7)
+*/
+char name[20];
 
-char name[];
+void DisplayString(char *name)
+{
+	printf("Display name\n");
+	puts(name);
+}
 
 void main()
 {
-	printf("submit name");
+	printf("Input the name\n");
+	fflush(stdin);
+	gets(name);
+	DisplayString(name);
 }
